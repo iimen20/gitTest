87b158547f529ce283ed2e9b6a46e1ee511c85d7
ImenThabet <imen.thabet@outlook.com>
2018-12-23 16:10:27 -0500

    batta1

---
+++ batta/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace batta.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
